5a7c2e96c0a2132ddc4c609a6ba11aadcd60ac9b
phamngocthang <20110728@student.hcmute.edu.vn>
2023-09-30 09:29:30 -0700

    Code Structure

---
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/progamming/ShopmeBackEnd/ShopmeBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progamming.ShopmeBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopmeBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
