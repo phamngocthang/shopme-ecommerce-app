30a40ed8505b62669c7ffb413f307a6e99ec979a
phamngocthang <20110728@student.hcmute.edu.vn>
2023-10-01 14:03:47 +0700

    update code

---
+++ ShopmeWebParent/ShopmeFrontEnd/src/test/java/com/progamming/ShopmeFrontEnd/ShopmeFrontEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progamming.ShopmeFrontEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopmeFrontEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
