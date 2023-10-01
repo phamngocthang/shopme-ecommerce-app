99882bcb06bfb06ac8d5b32520452d75e640d631
phamngocthang <20110728@student.hcmute.edu.vn>
2023-10-02 00:48:22 +0700

    update code

---
+++ ShopmeCommon/src/main/java/com/progamming/ShopmeCommon/entity/Role.java
@@ -0,0 +1,19 @@
+package com.progamming.ShopmeCommon.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @Column(length = 40, nullable = false, unique = true)
+    private String name;
+    @Column(length = 150, nullable = false)
+    private String description;
+}
